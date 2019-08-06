0e12c37cb91d0ef01dd134710834d53f9bc0cfe2
ethanblumenthal <ethan.blumenthal@gmail.com>
2019-08-06 15:53:26 -0500

    implemented bubble sort algorithm

--- build/index.js
+++ build/index.js
@@ -1,2 +1,23 @@
 "use strict";
-console.log('hello');
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    var temp = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = temp;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
