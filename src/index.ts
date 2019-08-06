70748cadc1fddbd7ffc851c0d48f8210ef58242a
ethanblumenthal <ethan.blumenthal@gmail.com>
2019-08-06 17:08:48 -0500

    implemented linked list class

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 // const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 // const sorter = new Sorter(numbersCollection);
 
-const charactersCollection = new CharactersCollection('Xaayb');
-const sorter = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection('Xaayb');
+// const sorter = new Sorter(charactersCollection);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
